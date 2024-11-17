8cda8bf01ab5ee50a54b8e170c0ab5a148727847
penscom <clebiodesouza22@gmail.com>
2024-11-17 14:57:17 -0300

    .

--- app/front_end/next.config.mjs
+++ app/front_end/next.config.mjs
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+export default {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:3000/api/:path*',
+            },
+        ];
+    },
 };
 
-export default nextConfig;
